dd0abfd9c8a871999db51c1cbcf193208edd37f4
garmatt <mattia.garassini@gmail.com>
2019-03-06 02:06:02 +0100

    Added pictures and styles.

---
+++ public/scripts/game/angular/board-point.js
@@ -0,0 +1,85 @@
+﻿angular.module('gameEngine').directive('boardPoint', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            point: '='
+        },
+        link: function (scope) {
+
+            var GetBackgroundSource = function (x, y, board) {
+                var size = board.Size;
+                var result = 'point';
+
+                if (y === 1)
+                    result += '-bottom';
+                else if (y === size)
+                    result += '-top';
+
+                if (x === 1)
+                    result += '-left';
+                else if (x === size)
+                    result += '-right';
+
+                if (result !== 'point')
+                    return result;
+
+                var cornerStarLocation = 3;
+                if (board.IsSizeLarge)
+                    cornerStarLocation = 4;
+
+                if ((x === cornerStarLocation || x === size - cornerStarLocation + 1) && (y === cornerStarLocation || y === size - cornerStarLocation + 1))
+                    return result + '-star';
+
+                if (board.IsSizeOdd)
+                {
+                    var sideStarLocation = (size + 1) / 2;
+                    if (x === sideStarLocation && y === sideStarLocation)
+                        return result + '-star';
+
+                    if (board.IsSizeLarge)
+                    {
+                        if (((x === cornerStarLocation || x === size - cornerStarLocation + 1) && y === sideStarLocation)
+                            || ((y === cornerStarLocation || y === size - cornerStarLocation + 1) && x === sideStarLocation))
+                            return result + '-star';
+                    }
+                }
+
+                return result;
+            };
+
+            scope.pointClass = ['tile', 'empty'];
+
+            if (scope.point.Board.NextToPlay)
+                scope.stone = 'stone-' + scope.point.Board.NextToPlay;
+
+            scope.background = GetBackgroundSource(scope.point.X, scope.point.Y, scope.point.Board);
+
+            scope.$watch('point.Board.NextToPlay', function (newValue, oldValue) {
+                if (newValue && $.inArray('empty', scope.pointClass) >= 0)
+                    scope.stone = 'stone-' + newValue;
+            });
+
+            scope.$watch('point.Group.Board', function (newValue, oldValue) {
+                if (newValue) {
+                    scope.pointClass = 'point';
+                    scope.stone = 'stone-' + scope.point.Group.Color;
+                }
+                else {
+                    scope.pointClass = ['point', 'empty'];
+                }
+            });
+
+            scope.$watch('point.Board.KoPoint', function (newValue, oldValue) {
+                if (scope.point.Equals(newValue))
+                    scope.background += '-ko';
+
+                if (scope.point.Equals(oldValue))
+                    scope.background = GetBackgroundSource(scope.point.X, scope.point.Y, scope.point.Board);
+            });
+
+        },
+        templateUrl: '/Scripts/Game/angular/templates/board-point.html'
+        //template: "<div ng-class='pointClass'><img ng-src='/Pictures/{{background}}.jpg' /><div class='stone'><img ng-src='/Pictures/{{stone}}.png' /></div></div>"
+    };
+});
+
---
+++ public/scripts/game/angular/board.js
@@ -0,0 +1,93 @@
+﻿angular.module('gameEngine').directive('board', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            size: '='
+        },
+        link: function (scope) {
+            scope.Board = new Board(scope.size);
+            scope.Board.NextToPlay = Color.Black;
+            scope.GetIndices = function () {
+                return new Array(scope.size);
+            };
+            scope.GameHistory = [];
+            scope.MoveIndex = -1;
+            scope.StepForward = function () {
+                scope.GameHistory[++scope.MoveIndex].Do();
+                scope.Board.ToggleColor();
+                scope.Board.SelectedPoint = scope.GameHistory[scope.MoveIndex].PlayedStone;
+                scope.Board.KoPoint = scope.GameHistory[scope.MoveIndex].KoPoint;
+            };
+            scope.StepBackward = function () {
+                scope.GameHistory[scope.MoveIndex--].Undo();
+                scope.Board.ToggleColor();
+                scope.Board.SelectedPoint = scope.GameHistory[scope.MoveIndex].PlayedStone;
+                scope.Board.KoPoint = scope.GameHistory[scope.MoveIndex].KoPoint;
+            };
+        },
+        controller: ['$scope', function BoardController($scope) {
+            $scope.MakeMove = function (x, y) {
+                console.log('-------------');
+                var selectedPoint = $scope.Board.GetPoint(x, y);
+                if (selectedPoint.Group || selectedPoint.Equals($scope.Board.KoPoint)) {
+                    return;
+                }
+                var color = $scope.Board.NextToPlay;
+                console.log('Playing ' + selectedPoint + ' ' + color);
+                var neighbors = selectedPoint.GetNeighbors();
+                var neighboringGroupsOfOppositeColor = neighbors.filter(function (neighbor) {
+                    return neighbor.Group && neighbor.Group.Color !== color;
+                }).map(function (neighbor) {
+                    return neighbor.Group;
+                    }).unique();
+                console.log('Neighboring groups of opposite color: ' + neighboringGroupsOfOppositeColor.join('; '));
+                var isMoveLegal = neighboringGroupsOfOppositeColor.some(function (group) { return group.GetLiberties() <= 1; });
+                var newGroup = new Group(color);
+                newGroup.AddStone(selectedPoint, true);
+                console.log('New group: ' + newGroup);
+                var neighboringGroupsOfSameColor = neighbors.filter(function (neighbor) {
+                    return neighbor.Group && neighbor.Group.Color === color;
+                }).map(function (neighbor) {
+                    return neighbor.Group;
+                }).unique();
+                console.log('Neighboring groups of same color: ' + neighboringGroupsOfSameColor.join('; '));
+                var connectedGroup = $scope.Board.GetConnectedGroup(neighboringGroupsOfSameColor.concat([newGroup]));
+                console.log('Connected group: ' + connectedGroup);
+                var connectedGroupLiberties = connectedGroup.GetLiberties();
+                isMoveLegal = isMoveLegal || (connectedGroupLiberties > 0);
+                if (!isMoveLegal) {
+                    selectedPoint.Group = null;
+                    return;
+                }
+                var move = new Move($scope.Board, color, selectedPoint);
+                neighboringGroupsOfSameColor.forEach(function (groupToRemove) {
+                    $scope.Board.RemoveGroup(groupToRemove, move);
+                });
+                $scope.Board.AddGroup(connectedGroup, move);
+                console.log('Added group: ' + connectedGroup);
+                var lookForKo = connectedGroup.Stones.length === 1 && connectedGroupLiberties === 0;
+                var koPoint = null;
+                neighboringGroupsOfOppositeColor.forEach(function (groupToCheckRemove) {
+                    if (groupToCheckRemove.GetLiberties() < 1) {
+                        if (lookForKo && groupToCheckRemove.Stones.length === 1)
+                            koPoint = groupToCheckRemove.Stones[0];
+                        else
+                            koPoint = null;
+                        $scope.Board.RemoveGroup(groupToCheckRemove, move);
+                        lookForKo = false;
+                    }
+                });
+                $scope.Board.ToggleColor();
+                console.log(' ');
+                $scope.Board.SelectedPoint = selectedPoint;
+                $scope.Board.KoPoint = koPoint;
+                move.KoPoint = koPoint;
+                $scope.MoveIndex++;
+                $scope.GameHistory.splice($scope.MoveIndex, $scope.GameHistory.length - $scope.MoveIndex, move);
+            };
+        }],
+        templateUrl: '/Scripts/Game/angular/templates/board.html'
+        //template: "<table class='goban playable' cellspacing='0'><tbody><tr ng-repeat='y in GetIndices() track by $index'><td ng-repeat='x in GetIndices() track by $index'><board-point point='Board.GetPoint($index, size - 1 - $parent.$index)' ng-click='MakeMove($index, size - 1 - $parent.$index)'></board-point></td></tr></tbody></table>"
+    };
+});
+
