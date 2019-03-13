04c9caa2fee196ad38754ea2e08dbc3c9cd9aa24
garmatt <mattia.garassini@gmail.com>
2019-03-13 01:51:26 +0100

    Login improved

--- public/scripts/angular/translate-config.js
+++ public/scripts/angular/translate-config.js
@@ -2,12 +2,16 @@ angular.module('indigo').config(function ($translateProvider) {
   $translateProvider.translations('en', {
     WELCOME1: 'Welcome to Indigo, an online go playing portal.',
     WELCOME2: 'Playing on Indigo is completely free and does not require installing any software.',
-    WELCOME3: 'Have fun!'
+    WELCOME3: 'Have fun!',
+	LOGIN_USER: 'Username or email',
+	LOGIN_PASSWORD: 'Password'
   });
   $translateProvider.translations('it', {
     WELCOME1: 'Benvenuti su Indigo, una piattaforma per giocare a go online.',
     WELCOME2: 'Indigo è gratis al 100%. Non occorre installare alcun software per giocare.',
-    WELCOME3: 'Buon divertimento!'
+    WELCOME3: 'Buon divertimento!',
+	LOGIN_USER: 'Nome utente o indirizzo email',
+	LOGIN_PASSWORD: 'Password'
   });
   $translateProvider.preferredLanguage('en');
 });
