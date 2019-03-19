283750122b0fbf9fba9edafe51aecf4e9f40b3ff
Mattia Garassini <mattia.garassini@sedapta.com>
2019-03-19 17:49:05 +0100

    Signup controller

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get('/', function(req, res) {
     res.render('index', { size: 19});
 });
 
+router.get('/index', function(req, res) {
+    res.render('index', { size: 19});
+});
+
+router.get('/signup', function(req, res) {
+    res.render('signup');
+});
+
 module.exports = router;
